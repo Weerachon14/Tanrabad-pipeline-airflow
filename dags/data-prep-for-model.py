06182b760c45b9d0afe3a583f818474f5ca43af1
weerachon <weerachon.sun14@gmail.com>
2024-09-11 14:00:32 +0700

    Add Task 3 Transform label

--- dags/data-prep-for-model.py
+++ dags/data-prep-for-model.py
@@ -6,6 +6,8 @@ from airflow.operators.python_operator import PythonOperator
 from sshtunnel import SSHTunnelForwarder
 from pymongo import MongoClient
 
+#Function task 1
+ 
 def query_data_from_mongo(**kwargs):
     # ตั้งค่า SSH Tunnel เพื่อ Access Mongo
     with SSHTunnelForwarder(
@@ -37,6 +39,8 @@ def query_data_from_mongo(**kwargs):
         kwargs['ti'].xcom_push(key='mongo_file_path', value=file_path)
         
         return file_path  # ส่งพาธไฟล์กลับไปยัง XCom
+    
+#Function task 2
 
 def filter_data(**kwargs):
     # ดึง path ของไฟล์จาก XCom
@@ -79,7 +83,7 @@ def filter_data(**kwargs):
 
     return filtered_file_path
 
-# def check_json_file(**kwargs):
+# def check_json_file_task2(**kwargs):
 #     # ดึง path ของไฟล์จาก XCom
 #     ti = kwargs['ti']
 #     file_path = ti.xcom_pull(key='filtered_file_path', task_ids='filter_data')
@@ -96,6 +100,69 @@ def filter_data(**kwargs):
 #     except Exception as e:
 #         raise ValueError(f"Error reading JSON file: {e}")
 
+# def check_json_file_task3(**kwargs):
+#     # ดึง path ของไฟล์จาก XCom
+#     ti = kwargs['ti']
+#     file_path = ti.xcom_pull(key='transformed_label_file_path', task_ids='transform_label')
+
+#     # ตรวจสอบว่า file_path ได้ถูกดึงมาจาก XCom หรือไม่
+#     if not file_path:
+#         raise ValueError("file_path is None. Make sure the previous task has pushed the correct file path to XCom.")
+
+#     # อ่านข้อมูลจากไฟล์ JSON
+#     try:
+#         with open(file_path, 'r') as file:
+#             data = json.load(file)
+#             print("JSON data:", data)  # พิมพ์ข้อมูล JSON เพื่อตรวจสอบ
+#     except Exception as e:
+#         raise ValueError(f"Error reading JSON file: {e}")
+
+def transform_label(**kwargs):
+    ti = kwargs['ti']
+    file_path = ti.xcom_pull(key='filtered_file_path', task_ids='filter_data')
+    
+    if not file_path:
+        raise ValueError("file_path is None. Make sure the previous task has pushed the correct flie to XCom")
+
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+      
+    transformed_data = []
+    for item in data:
+        label_data = item.get('label', [])
+        # ตรวจสอบว่า label_data เป็น list หรือไม่
+        if label_data is None:
+            label_data = []
+        
+        # Transform L in label
+        for label in label_data:
+            if 'l' in label:
+                if label['l'].startswith('Y'):
+                    label['l'] = 1
+                elif label['l'].startswith('N'):
+                    label['l'] = 0
+    
+        transformed_field = {
+            'text': item.get('text'),
+            'text_cleaned': item.get('text_cleaned'),
+            'text_tokenized': item.get('text_tokenized'),
+            'label': label_data
+        }
+        transformed_data.append(transformed_field)
+        
+    transformed_label_file_path = '/opt/airflow/data/transformed_label_data.json'    
+    
+    directory = os.path.dirname(transformed_label_file_path) 
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        
+    with open(transformed_label_file_path, 'w') as transformed_label_file:
+        json.dump(transformed_data, transformed_label_file)
+                        
+    ti.xcom_push(key='transformed_label_file_path', value= transformed_label_file_path)
+    return transformed_label_file_path
+
+
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2024, 9, 11),
@@ -108,7 +175,8 @@ with DAG(dag_id = 'Filter_data',
          schedule_interval = None,
          catchup = False
 ) as dag:
-    # Task1
+    
+    # Task 1
     Query_mongo = PythonOperator(
         task_id = 'query_data_from_mongo',
         python_callable = query_data_from_mongo
@@ -116,15 +184,28 @@ with DAG(dag_id = 'Filter_data',
     )
     # Task 2
     Filter_task = PythonOperator(
-        task_id='filter_data',
-        python_callable=filter_data,
-        provide_context=True
+        task_id = 'filter_data',
+        python_callable = filter_data,
+        provide_context = True
     )
-
+    # Check data in Task 2
     # Check_json = PythonOperator(
     #     task_id='check_json_file',
     #     python_callable=check_json_file,
     #     trigger_rule='all_done'  # เพิ่ม trigger rule ตรงนี้
     # )
     
-Query_mongo >> Filter_task
+    # Task 3 
+    Transform_label_task = PythonOperator(
+        task_id = 'transform_label',
+        python_callable = transform_label,
+        provide_context = True
+    )
+    
+        # Check data in Task 3
+    # Check_json = PythonOperator(
+    #     task_id='check_json_file',
+    #     python_callable=check_json_file,
+    #     trigger_rule='all_done'  # เพิ่ม trigger rule ตรงนี้
+    # )
+Query_mongo >> Filter_task >> Transform_label_task
